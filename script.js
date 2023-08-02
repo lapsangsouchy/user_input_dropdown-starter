3c5f8096551c8b23d7e7e9f258f31511a159304c
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2023-08-01 21:45:29 -0400

    Completed Code Along

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 /* @TODO Get References */
+let menu = document.getElementById('menu');
+let button = document.querySelector('button');
+let screenImage = document.querySelector('img');
+let pokemonInfoDiv = document.getElementById('pokemon_info');
 
 /* @TODO Event Handler */
-
 /*  
     try updating when button is clicked vs when
     dropdown value changes with
     menu.onchange = 
 */
 
-// prevent the page from reloading
+button.onclick = function (event) {
+  // prevent the page from reloading
 
-// capture userChoice
+  event.preventDefault();
 
-// show Pokemon picture based on userChoice
+  // capture userChoice
+  let userChoice = menu.value;
+  console.log(userChoice);
 
-// change src of screenImage
-
-// change innerHTML of pokemonInfoDiv
+  // show Pokemon picture based on userChoice
+  if (userChoice == 'pika') {
+    // change src of screenImage
+    screenImage.src = 'images/pikachu.png';
+    // change innerHTML of pokemonInfoDiv
+    pokemonInfoDiv.innerHTML = 'a yellow mouse';
+  } else if (userChoice == 'bulba') {
+    screenImage.src = 'images/bulbasaur.png';
+    pokemonInfoDiv.innerHTML = 'a green seed';
+  } else if (userChoice == 'char') {
+    screenImage.src = 'images/charmander.png';
+    pokemonInfoDiv.innerHTML = 'an orange chameleon';
+  } else if (userChoice == 'squir') {
+    screenImage.src = 'images/squirtle.png';
+    pokemonInfoDiv.innerHTML = 'a blue turtle';
+  }
+};
